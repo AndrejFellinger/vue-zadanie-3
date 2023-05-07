2b6552ea39d17dc6c96ed499c27d70370e7e093e
Andrej Fellinger <fellinger.andrej@gmail.com>
2023-05-07 14:39:47 +0200

    update

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import AboutView from './views/AboutView.vue'
+
 </script>
 
 <template>
--- vue-test/src/stores/todos.js
+++ vue-test/src/stores/todos.js
@@ -2,14 +2,12 @@ import { ref, computed, useTransitionState } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useTodosStore = defineStore('todos', {
-
+    
   state: () => {
-    // if(localStorage.getItem("todos")){
-    //   return JSON.parse(localStorage.getItem("todos"));
-    // }
+
     return {
-      todos: [],
-      deleted: [],
+      todos: JSON.parse(localStorage.getItem('todos')),
+      deleted: JSON.parse(localStorage.getItem('deleted')),
       editing: false,
       editingId: null,
       recoverId: null,
@@ -21,21 +19,27 @@ export const useTodosStore = defineStore('todos', {
   actions: {
     recoverTask(){
       this.todos.push(this.deleted.splice(this.recoverId, 1)[0])
+      localStorage.setItem('todos', JSON.stringify(this.todos))
+      localStorage.setItem('deleted', JSON.stringify(this.deleted))
     },
     deleteTask(){
       this.deleted.push(this.todos.splice(this.deleteId, 1)[0])
+      localStorage.setItem('todos', JSON.stringify(this.todos))
+      localStorage.setItem('deleted', JSON.stringify(this.deleted))
     },
     addTask(){
       if(this.editing == false && this.input != '') {
 
         this.todos.push(this.input)
-        this.input = '' 
+        this.input = ''
+        localStorage.setItem('todos', JSON.stringify(this.todos))
 
       }if(this.editing == true && this.input != '') {
 
         this.todos[this.editingId] = this.input
         this.editing = false
         this.input = ''
+        localStorage.setItem('todos', JSON.stringify(this.todos))
 
       }
     },
